9f440510187d0107df29f16d06fc8536aacf0e3b
makibishi <makibishi0212@gmail.com>
2021-02-07 03:25:54 +0900

    add groups method

--- src/data_structure/unionfind.rs
+++ src/data_structure/unionfind.rs
@@ -53,6 +53,29 @@ impl UnionFind {
     pub fn same(&mut self, x: usize, y: usize) -> bool {
         self.root(x) == self.root(y)
     }
+
+    pub fn groups(&mut self) -> Vec<Vec<usize>> {
+        let n = self.size.len();
+        let mut root_buf = vec![0; n];
+        let mut group_size = vec![0; n];
+        for i in 0..n {
+            root_buf[i] = self.root(i);
+            group_size[root_buf[i]] += 1;
+        }
+
+        let mut result = vec![Vec::new(); n];
+        for i in 0..n {
+            result[i].reserve(group_size[i]);
+        }
+        for i in 0..n {
+            result[root_buf[i]].push(i);
+        }
+
+        result
+            .into_iter()
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<Vec<usize>>>()
+    }
 }
 
 #[test]
@@ -73,6 +96,7 @@ fn union_find_test() {
     assert_eq!(group.same(0, 3), true);
     assert_eq!(group.same(1, 3), true);
     assert_eq!(group.same(0, 4), true);
+    assert_eq!(group.groups(), vec![vec![0, 1, 3, 4], vec![2]]);
 
     let mut group2 = UnionFind::new(200);
     (0..200).step_by(2).for_each(|x| {
