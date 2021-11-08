57b58f5170f3667b8da75904e35bdc92d0ab19e0
菊池健吾 <kikuchikengo@kikuchikengonoMacBook-Pro.local>
2021-11-09 06:05:16 +0900

    add rnorm

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -1,1 +1,2 @@
 pub mod norm_inv;
+pub mod rnorm;
---
+++ src/stats/rnorm.rs
@@ -0,0 +1,14 @@
+use rand::{prelude::StdRng, Rng, SeedableRng};
+
+use cargo_snippet::snippet;
+use rand;
+
+// must rand=0.7.3
+// box-muller法による標準正規分布に従った乱数
+#[snippet("@rnorm")]
+pub fn rnorm() -> f64 {
+    let mut rng = rand::thread_rng();
+    let x: f64 = rng.sample(rand::distributions::Standard);
+    let y: f64 = rng.sample(rand::distributions::Standard);
+    return (-2.0 * (1.0 - x).ln()).sqrt() * (2.0 * std::f64::consts::PI * y).cos();
+}
