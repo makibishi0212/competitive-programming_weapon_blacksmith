063388f5d47d83e8f8aa10bd654389b0a3fd9149
makibishi <makibishi0212@gmail.com>
2021-11-14 15:51:53 +0900

    add snippet

--- src/util/shuffle_vec.rs
+++ src/util/shuffle_vec.rs
@@ -1,5 +1,7 @@
+use cargo_snippet::snippet;
 use rand::*;
 
+#[snippet("@shuffle_vec")]
 pub fn shuffle_vec<T: Copy>(vec: &mut [T]) {
     for i in 0..vec.len() {
         let mut tmp: usize = random();
